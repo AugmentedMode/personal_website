88123b7ca2fe66d6defe96ba33199a6959a82706
Jacob Lebowitz <augmentedmode@jacobs-mbp-5.home>
2019-11-09 15:00:28 -0500

    added experiences

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
